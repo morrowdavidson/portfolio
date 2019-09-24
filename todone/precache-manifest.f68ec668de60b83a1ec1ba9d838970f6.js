829ca9ee98b3389e3da6a63e954037e437478ffc
Morrow Davidson <morrow.davidson@gmail.com>
2019-09-24 12:24:40 -0500

    Add todone app

---
+++ todone/precache-manifest.f68ec668de60b83a1ec1ba9d838970f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91e12a64480111cd0d2d870881fe9902",
+    "url": "/static/media/todone_logo.91e12a64.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "cd5e2d0de2dcf411a4a2",
+    "url": "/static/js/main.7b2df697.chunk.js"
+  },
+  {
+    "revision": "c69ca85d526c3c87f830",
+    "url": "/static/js/2.609099d6.chunk.js"
+  },
+  {
+    "revision": "cd5e2d0de2dcf411a4a2",
+    "url": "/static/css/main.6ef554c5.chunk.css"
+  },
+  {
+    "revision": "0ada161950be96e2e2d3bb54f8f444b7",
+    "url": "/index.html"
+  }
+];
